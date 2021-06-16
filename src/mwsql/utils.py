377c6d4378ec28ecc281f02bab1988e7ab857210
Slavina Stefanova <slavina.stefanova@gmail.com>
2021-06-16 15:29:03 +0200

    Improve file handling

--- src/mwsql/utils.py
+++ src/mwsql/utils.py
@@ -1,132 +1,87 @@
-'''Helper functions used in src/dump.py'''
-
 import gzip
-import re
 import sys
+import wget
 
+from pathlib import Path
+from urllib.error import HTTPError
 
-from typing import List
-
-
-def head(file_path, n_lines=10):
-    '''Display top of compressed file, similar to `zcat | head` in GNU'''
-
-    with gzip.open(file_path, 'rt', encoding='utf-8') as infile:
-        for line in infile:
-            print(line.strip())
-            n_lines -= 1
-            if n_lines == 0:
-                break
-
-
-# mwsql helper functions
-def is_insert_statement(line: str) -> bool:
-    '''Check whether a string is an SQL `insert into` statement.'''
-
-    return line.startswith('INSERT INTO')
-
-
-def is_create_statement(line: str) -> bool:
-    '''Check whether a string is an SQL `create table` statement.'''
-
-    return line.startswith('CREATE TABLE')
-
-
-def get_table_name(line: str) -> str:
-    '''Extract SQL table name from string'''
-
-    table_name_pattern = r'`([\S]*)`'
-    table_name = re.search(table_name_pattern, line).group(1)
-    return table_name
-
-
-def has_col_name(line: str) -> bool:
-    '''Check whether a string contains an SQL column name'''
-
-    return line.strip().startswith('`')
-
-
-def get_col_name(line: str) -> str:
-    '''Extract SQL column names and data types from string'''
-
-    col_name_pattern = r'`([\S]*)`'
-    col_name = re.search(col_name_pattern, line).group(1)
-
-    # I was going to suggest trying to map SQL dtypes to numpy or native dtypes in Python
-    # but then I feel like most libraries like Pandas auto-detect and do this for you
-    # so probably not necessary.
-    col_dtype_pattern = r'` ((.)*),'
-    col_dtype = re.search(col_dtype_pattern, line).group(1)
-
-    return col_name, col_dtype
-
-
-def has_primary_key(line: str) -> str:
-    '''Check whether a string contains an SQL primary key'''
-
-    return line.strip().startswith('PRIMARY KEY')
-
-
-def get_primary_key(line: str) -> str:
-    '''Extract SQL table primary key from string'''
-
-    pattern = r'`([\S]*)`'
-    primary_key = re.search(pattern, line).group(1).replace('`', '').split(',')
-
-    return primary_key
-
-
-def parse_sql_stmt(line: str) -> List[List[str]]:
-    '''Parse an SQL INSERT INTO statement into a list
-    of lists representing the rows in the table.
+def open_file(file_path, mode, encoding=None):
+    '''Open file and return a file handle. Works with both
+    gzipped and uncompressed files.
     '''
 
-    out = []
-    tup = []
-    field = []
+    if 'b' in mode and encoding is not None:
+        raise ValueError("Argument 'encoding' not supported in binary mode")
 
-    in_tuple = False
-    in_quote = False
+    if str(file_path).endswith('.gz'):
+        with gzip.open(file_path, mode, encoding=encoding) as file_handle:
+            yield from file_handle
+    else:
+        with gzip.open(file_path, mode, encoding=encoding) as file_handle:
+            yield from file_handle
 
-    for i in range(1, len(line) -1):
-        prev = line[i-1]
-        curr = line[i]
 
-        if in_tuple and curr not in "()',":
-            field.append(curr)
+def head(file_path, n_lines=10, encoding='utf-8'):
+    '''Display first n lines of a file. Works with both
+    gzipped and uncompressed files. Defaults to 10 lines.
+    '''
 
-        elif curr == '(':
-            if not in_quote:
-                in_tuple = True
-            elif in_quote:
-                field.append(curr)
+    if str(file_path).endswith('.gz'):
+        infile = open_file(file_path, 'rt', encoding=encoding)
+    else:
+        infile = open_file(file_path, 'r', encoding=encoding)
 
-        elif curr == ')':
-            if not in_quote:
-                in_tuple = False
-            else:
-                field.append(curr)
+    for line in infile:
+        if n_lines == 0:
+            break
+        try:
+            print(line.strip())
+            n_lines -= 1
+        except StopIteration:
+            return
+    return
 
-        elif curr == "'":
-            if not in_quote:
-                in_quote = True
-            elif in_quote:
-                if prev != '/':
-                    in_quote = False
 
-        elif curr == ',':
-            if not in_quote and not in_tuple:
-                tup.append(''.join(field))
-                out.append(tup)
-                field = []
-                tup = []
-            elif in_quote:
-                field.append(curr)
-            elif in_tuple and not in_quote:
-                tup.append(''.join(field))
-                field = []
+def progress_bar(current, total, width=60):
+    '''Custom progress bar for wget downloads'''
 
-    tup.append(''.join(field))
-    out.append(tup)
-    return out
+    unit = 'bytes'
 
+    # Show file size in MB for large files
+    if total >= 100000:
+        MB = 1024 * 1024
+        current = current / MB
+        total = total / MB
+        unit = 'MB'
+
+    progress = current / total
+    progress_message = f"Progress: {progress:.0%} [{current:.1f} / {total:.1f}] {unit}"
+    sys.stdout.write('\r' + progress_message)
+    sys.stdout.flush()
+
+
+def load(db, filename):
+    '''Load dump file from public dir if in PAWS, else download
+    from the web. Returns a file object.
+    '''
+
+    PAWS_ROOT_DIR = Path('/public/dumps/public/')
+    DUMPS_URL = 'https://dumps.wikimedia.org/'
+    subdir = Path(db, 'latest')
+    extended_filename = f'{db}-latest-{filename}.sql.gz'
+    file_path = Path(extended_filename)
+
+    if PAWS_ROOT_DIR.exists():
+        dump_file = Path(PAWS_ROOT_DIR, subdir, file_path)
+
+    else:
+        subdir, file_path = str(subdir), str(file_path)
+        url = f'{DUMPS_URL}{subdir}/{file_path}'
+        try:
+            print(f'Downloading {url}')
+            dump_file = wget.download(url, bar=progress_bar)
+        except HTTPError:
+            print('File not found')
+            return None
+
+    return dump_file
