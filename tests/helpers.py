1cc62af20ef2a00f49962904a259c892526e3dd0
Slavina Stefanova <slavina.stefanova@gmail.com>
2021-07-19 14:12:10 +0200

    Create separate file for test utils

---
+++ tests/helpers.py
@@ -0,0 +1,15 @@
+import sys
+from io import StringIO
+
+
+# Helper class for capturing stout
+class Capturing(list):
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = StringIO()
+        return self
+
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio  # free up some memory
+        sys.stdout = self._stdout
