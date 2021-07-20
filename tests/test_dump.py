97532e82a8b7e950fc6275916310989961b379aa
Slavina Stefanova <slavina.stefanova@gmail.com>
2021-07-20 10:08:59 +0200

    Add tests for dump.py

--- tests/test_dump.py
+++ tests/test_dump.py
@@ -5,7 +5,7 @@ import pytest
 
 from mwsql.dump import Dump
 
-# from .helpers import Capturing
+from .helpers import Capturing
 
 CURRENT_DIR = Path(__file__).parent
 DATA_DIR = CURRENT_DIR.parent / "data"
@@ -77,7 +77,23 @@ def test_dtypes(dump_gz):
     )
 
 
-expected_out = [
+def test_rows_unconverted(dump_gz):
+    rows = dump_gz.rows(convert_dtypes=False)
+    first = next(rows)
+    assert first == ["1", "mw-replace", "0", "10200"]
+    second = next(rows)
+    assert second == ["2", "visualeditor", "0", "305860"]
+
+
+def test_rows_converted(dump_gz):
+    rows = dump_gz.rows(convert_dtypes=True)
+    first = next(rows)
+    assert first == [1, "mw-replace", 0, 10200]
+    second = next(rows)
+    assert second == [2, "visualeditor", 0, 305860]
+
+
+expected_out_unconverted = [
     "['ctd_id', 'ctd_name', 'ctd_user_defined', 'ctd_count']",
     "['1', 'mw-replace', '0', '10200']",
     "['2', 'visualeditor', '0', '305860']",
@@ -106,26 +122,19 @@ expected_out_converted = [
 ]
 
 
-def test_rows():
-    pass
+def test_head_unconverted(dump_gz):
+    with Capturing() as output:
+        dump_gz.head(10, convert_dtypes=False)
+    assert output == expected_out_unconverted
 
 
-# @pytest.mark.parametrize(
-#     "n_lines,convert,expected",
-#     [
-#         (5, False, expected_out[:5]),
-#         (5, True, expected_out_converted[:5]),
-#         (10, False, expected_out[:10]),
-#         (10, True, expected_out_converted[:10]),
-#     ],
-# )
-# def test_head(n_lines, convert, expected):
-#     with Capturing() as output:
-#         dump_gz.head(n_lines, convert)
-#     assert output == expected
+def test_head_converted(dump_gz):
+    with Capturing() as output:
+        dump_gz.head(10, convert_dtypes=True)
+    assert output == expected_out_converted
 
 
-def test_head_no_exception(dump_gz, dump_unzipped):
+def test_head_does_not_raise_exception(dump_gz, dump_unzipped):
     try:
         dump_gz.head(200)
         dump_unzipped.head(200)
@@ -135,7 +144,6 @@ def test_head_no_exception(dump_gz, dump_unzipped):
 
 def test_to_csv(dump_gz):
     csv_filepath = CURRENT_DIR / "testfile.csv"
-    # write CSV file
     dump_gz.to_csv(csv_filepath)
     with open(csv_filepath) as infile:
         content = infile.readlines()
